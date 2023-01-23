bc5232360daa9c1e0a438dbbf1d7a34b428e13c4
cylify <mradi.lachman@gmail.com>
2023-01-22 22:32:29 -0500

    .

--- View/UIBattleScreen.java
+++ View/UIBattleScreen.java
@@ -1,12 +1,34 @@
 import java.awt.GridLayout;
-
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class UIBattleScreen {
+public class UIBattleScreen extends JPanel {
+    JButton fight,bag,run,pokemon;
+    GridBagConstraints c = new GridBagConstraints();
+
     public UIBattleScreen() {
-        JButton fight = new JButton();
-        JButton bag = new JButton();
-        JButton pokemon = new JButton();
-        JButton run = new JButton();
+        setLayout(new GridLayout());
+        c.insets = new Insets(5,5,5,5);
+        fight = new JButton("FIGHT");
+        c.gridx = 0;
+        c.gridy = 0;
+        add(fight, c);
+
+        bag = new JButton("BAG");
+        c.gridx = 1;
+        c.gridy = 1;
+        add(bag, c);
+
+        run = new JButton("RUN");
+        c.gridx = 2;
+        c.gridy = 2;
+        add(run, c);
+        pokemon = new JButton("POK\u00E9MON");
+        c.gridx = 3;
+        c.gridy = 3;
+        add(pokemon, c);
+
     }
 }
--- View/UIStartScreen.java
+++ View/UIStartScreen.java
@@ -6,28 +6,26 @@ import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.CardLayout;
+import java.awt.GridBagConstraints;
 
-public class UIStartScreen extends JFrame {
+public class UIStartScreen extends JPanel {
+    CardLayout c;
+    JPanel main;
 
-    static JPanel main;
-    static CardLayout c;
-
-    public UIStartScreen() {
-        super("Pok\uEE09mon");
-        c = new CardLayout();
-        main = new JPanel(c);
+    public UIStartScreen(CardLayout c, JPanel main) {
+        this.c = c;
+        this.main = main;
 
         ImageIcon bgImg = new ImageIcon("C:/Users/mradi/Dropbox/Programming/Java/Grade 12 Computer Science/Unit 4/Pokemon/Assets/StartBackground.png");
         JButton play = new JButton(bgImg);
         play.setMargin(new Insets(0,0,0,0));
-        main.add(play);
-        add(main);
-        pack();
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(play);
+        play.addActionListener(e -> showBattleScreen());
     }
 
-    public static void main(String[] args) {
-        new UIStartScreen();
+    public void showBattleScreen() {
+        UIBattleScreen battleScreen = new UIBattleScreen();
+        main.add(battleScreen, "battle");
+        c.show(battleScreen, "battle");
     }
 }
