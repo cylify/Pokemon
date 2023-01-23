1c56568af9f922629d482e44799af910fd0dab3e
cylify <mradi.lachman@gmail.com>
2023-01-22 21:51:33 -0500

    df

---
+++ View/UIBattleScreen.java
@@ -0,0 +1,12 @@
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+
+public class UIBattleScreen {
+    public UIBattleScreen() {
+        JButton fight = new JButton();
+        JButton bag = new JButton();
+        JButton pokemon = new JButton();
+        JButton run = new JButton();
+    }
+}
